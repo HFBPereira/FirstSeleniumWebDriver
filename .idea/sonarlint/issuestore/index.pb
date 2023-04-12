e4fe96a10279d0220542a90d0a1fadd378253602
Henrique Borges <hfbpereira@gmail.com>
2023-04-12 20:56:54 -0300

    3rd commit

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,3 +6,15 @@ S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+Å
+Q.idea/sonarlint/securityhotspotstore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,3\0\30205a209c6ce77c4cfc19a09e6f57c588b7fb88
+w
+G.idea/sonarlint/issuestore/9/f/9fe84ebb15faf917b7def6236dba604453cc61e0,f\0\f0e0f9cec86fe4b939ae22be5264c758801eb28f
+w
+G.idea/sonarlint/issuestore/8/8/88a9255124c95bdc913197c120a8d560edc59c8e,a\e\ae9ddf453f55affea2d6b9e3973cac1eb2efb4e1
+Å
+Q.idea/sonarlint/securityhotspotstore/8/8/88a9255124c95bdc913197c120a8d560edc59c8e,9\0\909762385fc6eb20f922a91c6865741308b57f2c
+Å
+Q.idea/sonarlint/securityhotspotstore/9/f/9fe84ebb15faf917b7def6236dba604453cc61e0,b\b\bbf202272a96de5cd6b6828ffdc55fb3ce45289c
+w
+G.idea/sonarlint/issuestore/a/5/a5cc2925ca8258af241be7e5b0381edf30266302,e\d\ed67eb178d523a4b3f117dcbe0eda5196367ceec
---
+++ src/main/java/ButtonsPratica.java
@@ -0,0 +1,123 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ButtonsPratica {
+    private final static String URL = "https://igorsmasc.github.io/botoes_atividade_selenium/";
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @BeforeEach
+    public void setupEach() {
+        driver.get(URL);
+    }
+
+    @AfterAll
+    public static void setdownEach() {
+        driver.quit();
+    }
+
+    @Test
+    public void clickComID() {
+        WebElement captureButton = driver.findElement(By.id("button1"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 1 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComClassName() {
+        WebElement captureButton = driver.findElement(By.className("button-2"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 2 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComName() {
+        WebElement captureButton = driver.findElement(By.name("button3"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 3 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComCSS() {
+        WebElement captureButton = driver.findElement(By.cssSelector("button.button-4"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 4 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComXPATH() {
+        WebElement captureButton = driver.findElement(By.xpath("//*[@id=\"button5\"]"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 5 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComLIVRE() {
+        WebElement captureSwitch = driver.findElement(By.id("button6-switch"));
+        captureSwitch.click();
+
+        WebElement captureButton = driver.findElement(By.id("button6"));
+        captureButton.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O botão 6 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComLinkText() {
+        WebElement captureLink = driver.findElement(By.linkText("Link 1"));
+        captureLink.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O link 1 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComPartialLinkText() {
+        WebElement captureLink = driver.findElement(By.partialLinkText("2"));
+        captureLink.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O link 2 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+    @Test
+    public void clickComLivre() {
+        WebElement captureLink = driver.findElement(By.id("link3"));
+        captureLink.click();
+
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("O link 3 foi selecionado.", alert.getText());
+        alert.accept();
+    }
+
+}
